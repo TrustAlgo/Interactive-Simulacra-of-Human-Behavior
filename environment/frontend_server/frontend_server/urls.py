efa8f9c2186da88d69bb2f7ede63b6a832df04aa
Trust AI <stackweaver7@gmail.com>
2026-03-04 21:28:29 +0900

    Update urls.py

--- environment/frontend_server/frontend_server/urls.py
+++ environment/frontend_server/frontend_server/urls.py
@@ -1,35 +1,50 @@
-"""frontend_server URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.conf.urls import include, url
-from django.urls import path
 from django.contrib import admin
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
 from translator import views as translator_views
 
+
 urlpatterns = [
-    url(r'^$', translator_views.landing, name='landing'),
-    url(r'^simulator_home$', translator_views.home, name='home'),
-    url(r'^demo/(?P<sim_code>[\w-]+)/(?P<step>[\w-]+)/(?P<play_speed>[\w-]+)/$', translator_views.demo, name='demo'),
-    url(r'^replay/(?P<sim_code>[\w-]+)/(?P<step>[\w-]+)/$', translator_views.replay, name='replay'),
-    url(r'^replay_persona_state/(?P<sim_code>[\w-]+)/(?P<step>[\w-]+)/(?P<persona_name>[\w-]+)/$', translator_views.replay_persona_state, name='replay_persona_state'),
-    url(r'^process_environment/$', translator_views.process_environment, name='process_environment'),
-    url(r'^update_environment/$', translator_views.update_environment, name='update_environment'),
-    url(r'^path_tester/$', translator_views.path_tester, name='path_tester'),
-    url(r'^path_tester_update/$', translator_views.path_tester_update, name='path_tester_update'),
-    path('admin/', admin.site.urls),
+    # Landing
+    path("", translator_views.landing, name="landing"),
+
+    # Simulator
+    path("simulator_home/", translator_views.home, name="home"),
+
+    # Demo
+    path(
+        "demo/<str:sim_code>/<str:step>/<str:play_speed>/",
+        translator_views.demo,
+        name="demo",
+    ),
+
+    # Replay
+    path(
+        "replay/<str:sim_code>/<str:step>/",
+        translator_views.replay,
+        name="replay",
+    ),
+
+    path(
+        "replay_persona_state/<str:sim_code>/<str:step>/<str:persona_name>/",
+        translator_views.replay_persona_state,
+        name="replay_persona_state",
+    ),
+
+    # Environment
+    path("process_environment/", translator_views.process_environment, name="process_environment"),
+    path("update_environment/", translator_views.update_environment, name="update_environment"),
+
+    # Path tester
+    path("path_tester/", translator_views.path_tester, name="path_tester"),
+    path("path_tester_update/", translator_views.path_tester_update, name="path_tester_update"),
+
+    # Admin
+    path("admin/", admin.site.urls),
 ]
+
+# Serve static/media in development only
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
